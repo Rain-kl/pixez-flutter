b5a4ec59ac20c78f30e726b16b3c87cd88d5ee12
ryan <ryan@arctel.net>
2026-06-10 20:47:35 +0800

    移除旧 Server

--- server/migrations/20260606000003_drop_glance_illusts.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-DROP TABLE IF EXISTS glance_illusts;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
--- No rollback for dropping deprecated glance_illusts table
--- +goose StatementEnd
